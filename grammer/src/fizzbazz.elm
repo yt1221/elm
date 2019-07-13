6eb79922a3e16d8c0c6fde652095b4636780f9af
ゲッツ <>
2019-07-13 18:42:35 +0900

    if文学習 FizzBazzを作成

---
+++ grammer/src/fizzbazz.elm
@@ -0,0 +1,12 @@
+module FizzBazz exposing(fizzBazz)
+
+
+fizzBazz n =   
+    if remainderBy 15 n == 0 then
+        "FizzBazz"
+    else if remainderBy 3 n == 0 then
+        "Fizz"
+    else if remainderBy 5 n == 0 then
+        "Bazz"
+    else
+        String.fromInt n
