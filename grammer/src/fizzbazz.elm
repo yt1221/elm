869f6c64161589aad94a944d317cb01ac31285d4
ゲッツ <>
2019-07-13 19:02:00 +0900

    elm-formatを適用

--- grammer/src/fizzbazz.elm
+++ grammer/src/fizzbazz.elm
@@ -1,12 +1,15 @@
-module FizzBazz exposing(fizzBazz)
+module FizzBazz exposing (fizzBazz)
 
 
-fizzBazz n =   
+fizzBazz n =
     if remainderBy 15 n == 0 then
         "FizzBazz"
+
     else if remainderBy 3 n == 0 then
         "Fizz"
+
     else if remainderBy 5 n == 0 then
         "Bazz"
+
     else
         String.fromInt n
